de388f005690ae7b8aa3145259419397bb19e276
destroyer07 <junioranderline@gmail.com>
2016-07-28 16:05:59 -0300

    Atualizando

--- codigo/PWMWithADC.c
+++ codigo/PWMWithADC.c
@@ -82,7 +82,7 @@ void PWMConfigure(void) {
 void PWM_handler() {
 	
 	// Convertendo valor obtido pelo AD, na base 2^12, para base de 3000
-	PWMValue = ADCValue / 1.3653334; // ADCValue/(2^12)*3000
+	PWMValue = ADCValue / 1.3653334; // ADCValue/((2^12)/3000)
 
 	// Atualizando Duty Cicle
 	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1, (int)(PWMValue));
